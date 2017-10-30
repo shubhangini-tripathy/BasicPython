14e052d019e18e611dfdadc0a808b7f31d500977
Shubhangini Tripathy[D <shubhanginitripathy@gmail.com>
2017-10-31 00:45:40 +0530

    solved hackererth problem

--- monk_bdy_gift.py
+++ monk_bdy_gift.py
@@ -1,11 +1,21 @@
-a, b, c = map(int, input().split())
-temp = a
-while abs(temp) <= 109:
-    if temp == b:
-        break
-    temp += c
+N = int(input())
+A = [int(x) for x in input().split()]
+B = int(input())
+S = set(A)
+count = 0
+for i in range(B):
+    C = [int(x) for x in input().split()]
+    if len(S- set(C))== 0:
+        count += 1
+print(count)
 
-if abs(temp) <= 109:
-    print("Yes")
-else:
-    print("No")
+    
+
+# a = [1,2,3,4,5]
+# b = [1,2,3,4,6]
+# for i in range(len(b)):
+#     if b[i] in a:
+#         print("True")
+    
+
+    
