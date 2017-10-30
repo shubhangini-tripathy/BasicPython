fa7787637e3ba2fe24a2a700cca2748071425f15
Shubhangini Tripathy[D <shubhanginitripathy@gmail.com>
2017-10-31 00:50:22 +0530

    solve hackerearth problem

---
+++ girls_proprty.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = [int(x) for x in input().split()]
+B = int(input())
+S = set(A)
+count = 0
+for i in range(B):
+    C = [int(x) for x in input().split()]
+    if len(S- set(C))== 0:
+        count += 1
+print(count)
---
+++ monk_and_prisoner.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = [int(x) for x in input().split()]
+x = []
+y = []
+z = []
+i = 1
+def is_index(i):
+    if a= i > A[i]:
+        x.append(max(a))
+    else:
+        x.append(-1)
+    if b = i< A[i]:
+        y.append(min(b))
+
+
+        
+
+
